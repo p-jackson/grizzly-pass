b5fe9f6bf5f21791f3e27533e712b18a115bd5d0
Philip Jackson <p-jackson@live.com>
2017-03-14 23:13:04 +1300

    Set up react testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { shallow } from "enzyme";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+const app = shallow(<App />);
+
+it("shows the project name in a heading", () => {
+  expect(app.find("h2").text()).toBe("grizzly-pass");
+});
+
+it("shows the url", () => {
+  expect(app.find("p").text()).toBe("https://grizzly-pass.surge.sh");
 });
